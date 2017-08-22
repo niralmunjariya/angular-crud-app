b7050cb9ec2f8d566fa3546c7f26d57e69d67313
Niral Munjariya <niralmunjariya@gmail.com>
2017-08-22 21:08:47 +0530

    Minor improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,5 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'v4 Demo App';
+  title = 'Demo App';
 }
